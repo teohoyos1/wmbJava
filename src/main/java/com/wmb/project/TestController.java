0b0a6e4234949f493dfca6e25873e69c296abbef
teohoyos1 <mateorojashoyos@gmail.com>
2023-01-20 19:01:08 -0500

    init commit

---
+++ src/main/java/com/wmb/project/TestController.java
@@ -0,0 +1,15 @@
+package com.wmb.project;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/home")
+public class TestController {
+
+    @GetMapping("/wave")
+    public String home(){
+        return "Hello World";
+    }
+}
